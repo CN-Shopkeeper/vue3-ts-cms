3c24abff7fb67ae4080fc89851a4fa1658b80c55
shopkeeper <734212732@qq.com>
2021-09-23 20:27:16 +0800

    feat(base-echart): use vue3 new feature: setup; package base-echart

---
+++ src/base-ui/echart/src/base-echart.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="base-echart">
+    <div ref="echartDivRef" :style="{ width: width, height: height }"></div>
+  </div>
+</template>
+
+// 尝试setup新特性
+<script lang="ts" setup>
+import { ref, onMounted, defineProps, withDefaults } from "vue";
+import * as echarts from "echarts";
+import { EChartsOption } from "echarts";
+
+// 定义props
+const props = withDefaults(
+  defineProps<{
+    options: EChartsOption;
+    width?: string;
+    height?: string;
+  }>(),
+  {
+    width: "100%",
+    height: "360px"
+  }
+);
+
+const echartDivRef = ref<HTMLElement>();
+
+onMounted(() => {
+  const echartInstance = echarts.init(echartDivRef.value!);
+  echartInstance.setOption(props.options);
+});
+</script>
+
+<style scoped></style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard">
-    <el-row gutter="10">
+    <el-row :gutter="10">
       <el-col :span="7">
         <sk-card title="分类商品数量(饼图)"></sk-card>
       </el-col>
@@ -11,9 +11,11 @@
         <sk-card title="分类商品数量(玫瑰图)"></sk-card>
       </el-col>
     </el-row>
-    <el-row gutter="10" class="content-row">
+    <el-row :gutter="10" class="content-row">
       <el-col :span="12">
-        <sk-card title="分类商品的销量"></sk-card>
+        <sk-card title="分类商品的销量">
+          <base-echart :options="options"></base-echart>
+        </sk-card>
       </el-col>
       <el-col :span="12">
         <sk-card title="分类商品的收藏"></sk-card>
@@ -23,20 +25,43 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted } from "vue";
+import { defineComponent } from "vue";
 import { useStore } from "@/store";
 
 import SkCard from "@/base-ui/card";
+import BaseEchart from "@/base-ui/echart";
 
 export default defineComponent({
   name: "dashboard",
   components: {
-    SkCard
+    SkCard,
+    BaseEchart
   },
   setup() {
     const store = useStore();
     store.dispatch("dashboard/getDashboardDataAction");
-    return {};
+    const options = {
+      xAxis: {
+        type: "category",
+        data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+      },
+      yAxis: {
+        type: "value"
+      },
+      series: [
+        {
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: "bar",
+          showBackground: true,
+          backgroundStyle: {
+            color: "rgba(180, 180, 180, 0.2)"
+          }
+        }
+      ]
+    };
+    return {
+      options
+    };
   }
 });
 </script>
