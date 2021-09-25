ffe4994657199a035c943b444a2ad90deb5e025e
shopkeeper <734212732@qq.com>
2021-09-25 17:55:57 +0800

    docs: test github webhook

--- src/views/main/analysis/overview/overview.vue
+++ src/views/main/analysis/overview/overview.vue
@@ -4,6 +4,7 @@
       <div class="c-left">
         Vue3Admin 是基于 Vue3、Vuex、VueRouter、Vue CLI、 ElementPlus
         、TypeScript、Echart5 等后台系统解决方案。
+        该系统是Shopkeeper学习VUE3-TypeScript时，构建的项目。
       </div>
     </sk-card>
     <sk-card title="技术栈">
