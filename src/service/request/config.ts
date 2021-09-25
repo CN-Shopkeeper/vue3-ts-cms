afd43cd03f7b9a014e1bed8282172404d198387d
shopkeeper <734212732@qq.com>
2021-09-25 11:20:25 +0800

    fix: fix env variables

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -9,10 +9,10 @@ const TIME_OUT = 10000;
 
 if (process.env.NODE_ENV === "development") {
   BASE_URL = "/api";
-} else if (process.env.NODE_ENV === "development") {
-  BASE_URL = "production";
+} else if (process.env.NODE_ENV === "production") {
+  BASE_URL = "/api";
 } else {
-  BASE_URL = "test";
+  BASE_URL = "/api";
 }
 
 export { TIME_OUT, BASE_URL };
